1b9f4a7251035318cd746b0c7c63f78829298a9a
Daniel Zettis <zettisdaniel@gmail.com>
2022-05-02 22:59:23 +0200

    Update videoCap.py

--- videoCap.py
+++ videoCap.py
@@ -1,21 +1,32 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('traffic1.mp4')
+cap = cv2.VideoCapture('1.mp4')
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
 ret, frame1 = cap.read()
-ret, frame2 = cap.read()
+frame2 = frame1;
 
-while cap.isOpened():
+while True:
+    
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     _, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations = 6)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    cv2.drawContours(frame1, contours, -1, (0, 0, 255), 2)
+    
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        
+        if cv2.contourArea(contour) < 700:
+            continue
+        cv2.rectangle(frame1, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame1, "Status: {}".format('movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+    #cv2.drawContours(frame1, contours, -1, (0, 0, 255), 2)
+    
+    
+    
     
     cv2.imshow("feed", frame1)
     frame1 = frame2
